c65144c85bccb76e04cbd167b5d5e4069196a699
Gabriele Maurina <gabrielemaurina95@gmail.com>
2017-12-02 16:24:49 +0100

    V1 commit.

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app); 
+var io = require('socket.io')(server);
+
+app.use(express.static('resources'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/client.html');
+});
+
+io.on('connect', function(socket){
+	socket.on('lines', function(lines){
+		socket.broadcast.emit('lines', lines);
+	});
+});
+
+
+server.listen((process.env.PORT || 80));
