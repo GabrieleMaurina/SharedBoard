736390c5ea4ceccbff6d69b26e8f0db1f14d59ee
Gabriele Maurina <gabrielemaurina95@gmail.com>
2017-12-05 20:55:18 +0100

    Improved admin page.

--- resources/admin.js
+++ resources/admin.js
@@ -1,3 +1,5 @@
+var clientsTitle = document.getElementById('clients_title');
+var roomsTitle = document.getElementById('rooms_title');
 var clientsList = document.getElementById('clients_list');
 var roomsList = document.getElementById('rooms_list');
 
@@ -12,7 +14,7 @@ socket.on('update', function(clients){
 	var roomsHTML = '';
 	
 	for(i in clients){
-		clientsHTML += '<tr><td>' + clients[i].name + '</td><td>' + clients[i].ip + '</td><td>' + clients[i].room + '</td></tr>';
+		clientsHTML += '<tr><td>' + clients[i].name + '</td><td>' + clients[i].ip + '</td><td><a href="' + (clients[i].room || ADDRESS) + '">' + (clients[i].room || 'HOME') + '</a></td></tr>';
 		if(rooms[clients[i].room]){
 			rooms[clients[i].room].clientsCount++;
 		}
@@ -22,9 +24,11 @@ socket.on('update', function(clients){
 		}
 	}
 	for(i in rooms){
-		roomsHTML += '<tr><td>' + i + '</td><td>' + rooms[i].clientsCount + '</td></tr>';
+		roomsHTML += '<tr><td><a href="' + (i || ADDRESS) + '">' + (i || 'HOME') + '</a></td><td>' + rooms[i].clientsCount + '</td></tr>';
 	}
 	
+	clientsTitle.innerHTML = 'Clients ' + clients.length;
+	roomsTitle.innerHTML = 'Rooms ' + Object.keys(rooms).length;
 	clientsList.innerHTML = clientsHTML;
 	roomsList.innerHTML = roomsHTML;
 });
